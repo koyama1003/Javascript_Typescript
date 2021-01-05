186d965984fb5541984467c0534a61ad6c947cf2
古山 蒼二郎 <s_koyama@jupitor.co.jp>
2021-01-05 13:43:41 +0900

    first

--- dictionary/src/App.tsx
+++ dictionary/src/App.tsx
@@ -1,25 +1,23 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import jishoApi from "unofficial-jisho-api";
 
 const App: React.FC = () => {
-  const jishoApi = require("unofficial-jisho-api");
   const jisho = new jishoApi();
   const proxy = "https://cors-anywhere.herokuapp.com/";
   const [word, setWord] = useState<string>("");
   const url_phrase = proxy + jisho.getUriForPhraseSearch(word);
 
   const url_example = proxy + jisho.getUriForExampleSearch("令");
-  useEffect(() => {
-    const test = async () => {
-      const res = await axios.get(url_example);
 
-      const json = jisho.parseExamplePageHtml(res.data, "令");
-      console.log(json);
-    };
-    test();
-  });
+  const searchExample = async () => {
+    const res = await axios.get(url_example);
 
-  const searchWord = async () => {
+    const json = jisho.parseExamplePageHtml(res.data, "令");
+    console.log(json.results);
+  };
+
+  const searchPhrase = async () => {
     try {
       const res = await axios.get(url_phrase);
       console.log(res.data);
@@ -35,7 +33,8 @@ const App: React.FC = () => {
           setWord(e.target.value);
         }}
       />
-      <button onClick={searchWord}>search</button>
+      <button onClick={searchPhrase}>search</button>
+      <button onClick={searchExample}>search</button>
     </div>
   );
 };
