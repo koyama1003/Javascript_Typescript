7ab8ff30ff34d6342aaa635ccd42cb03897b6ff2
Sojiro Koyama <koyama10032009@outlook.jp>
2021-01-05 21:16:35 +0900

    Change:dictionary to revise reducer and store 1.5

--- dictionary/src/App.tsx
+++ dictionary/src/App.tsx
@@ -1,29 +1,35 @@
 import React, { useState } from "react";
 import axios from "axios";
 import jishoApi from "unofficial-jisho-api";
-
+import { useSelector, useDispatch } from "react-redux";
+import { searchExample, searchPhrase } from "./features/wordSlice";
+import { selectWord } from "./features/wordSlice";
 const App: React.FC = () => {
   const jisho = new jishoApi();
   const proxy = "https://cors-anywhere.herokuapp.com/";
   const [word, setWord] = useState<string>("");
   const url_phrase = proxy + jisho.getUriForPhraseSearch(word);
   const url_example = proxy + jisho.getUriForExampleSearch(word);
+  const words = useSelector(selectWord);
+  const dispatch = useDispatch();
 
-  const searchExample = async () => {
+  const getExample = async () => {
     try {
       const resHtml = await axios.get(url_example);
 
       const res = jisho.parseExamplePageHtml(resHtml.data, word);
       console.log(res.results);
+      dispatch(searchExample(res.results));
     } catch (err) {
       alert(err.message);
     }
   };
 
-  const searchPhrase = async () => {
+  const getPhrase = async () => {
     try {
       const res = await axios.get(url_phrase);
-      console.log(res.data);
+      console.log(res.data.data);
+      dispatch(searchPhrase(res.data.data));
     } catch (err) {
       alert(err.mesage);
     }
@@ -36,8 +42,8 @@ const App: React.FC = () => {
           setWord(e.target.value);
         }}
       />
-      <button onClick={searchPhrase}>Phrase</button>
-      <button onClick={searchExample}>Example</button>
+      <button onClick={getPhrase}>Phrase</button>
+      <button onClick={getExample}>Example</button>
     </div>
   );
 };
