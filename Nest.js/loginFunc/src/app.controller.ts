bab6126f94f6cc82032c0d9359788ec01f79c166
古山 蒼二郎 <s_koyama@jupitor.co.jp>
2021-02-01 14:37:23 +0900

    установил соединение с база данных с Nest.js

--- Nest.js/loginFunc/src/app.controller.ts
+++ Nest.js/loginFunc/src/app.controller.ts
@@ -9,8 +9,4 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
-  @Get()
-  sayHello(): string {
-    return this.appService.sayHello();
-  }
 }
---
+++ Nest.js/loginFunc/src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UsersService } from './users/users.service';
+import { UsersController } from './users/users.controller';
+import { UsersModule } from './users/users.module';
+import { User } from './users/entities/user.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3300,
+      username: 'root',
+      password: 'password',
+      database: 'testlogin',
+      entities: [User],
+      synchronize: true,
+    }),
+    UsersModule,
+  ],
+  controllers: [AppController, UsersController],
+  providers: [AppService, UsersService],
+})
+export class AppModule {}
