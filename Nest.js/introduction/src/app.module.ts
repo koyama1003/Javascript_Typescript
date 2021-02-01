43bcb5ee22e6b07b35d8a30d37f63c494825f996
古山 蒼二郎 <s_koyama@jupitor.co.jp>
2021-02-01 17:22:52 +0900

    выполняет введение Nest.js 2.01

---
+++ Nest.js/introduction/src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { CatsModule } from './cats/cats.module';
+import { LoggerMiddleware } from './common/middleware/logger.middleware';
+
+@Module({
+  imports: [CatsModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule implements NestModule {}
