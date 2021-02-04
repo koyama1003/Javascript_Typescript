ad995f902ec7e9f0a609ac48e79b8e3d09269ee7
Sojiro Koyama <koyama10032009@outlook.jp>
2021-02-04 20:53:40 +0900

    создал проект с Next.js

--- Nest.js/introduction/src/app.module.ts
+++ Nest.js/introduction/src/app.module.ts
@@ -1,12 +1,27 @@
-import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
-import { LoggerMiddleware } from './common/middleware/logger.middleware';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TestObjectModule } from './test-object/test-object.module';
+import { TestObject } from './test-object/test-object';
 
 @Module({
-  imports: [CatsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'izayoi1003',
+      database: 'typeorm',
+      entities: [TestObject],
+      synchronize: true,
+    }),
+    CatsModule,
+    TestObjectModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule implements NestModule {}
+export class AppModule {}
---
+++ Next.js/tutorial/pages/index.js
@@ -0,0 +1,8 @@
+import Layout from "../components/Layout";
+export default function Home() {
+  return (
+    <Layout title="home">
+      <p className="text-4xl">Hello Next.js</p>
+    </Layout>
+  );
+}
