ea1cf08421433d531b13ba078ca211451a5da20a
古山 蒼二郎 <s_koyama@jupitor.co.jp>
2021-02-04 14:44:06 +0900

    испытывал mobX

---
+++ React/mobX/src/js/TodoStore.js
@@ -0,0 +1,7 @@
+import { observable } from "mobx";
+class TodoStore {
+  @observable todos = ["buy milk", "buy eggs"];
+  @observable filter = "";
+}
+let store = (window.store = new TodoStore());
+export default store;
