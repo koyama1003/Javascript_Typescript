5fc4cb37f9203f34c5bfcc579508b27652f7d2d2
Sojiro Koyama <koyama10032009@outlook.jp>
2021-02-04 21:59:34 +0900

    учился getStaticProps getStaticPaths

--- Next.js/tutorial/pages/blog-page.js
+++ Next.js/tutorial/pages/blog-page.js
@@ -1,8 +1,23 @@
 import React from "react";
 import Layout from "../components/Layout";
+import { getAllPostsData } from "../lib/posts";
+import Post from "../components/Post";
 
-const Blog = () => {
-  return <Layout title="Blog">Blog</Layout>;
+const Blog = ({ posts }) => {
+  return (
+    <Layout title="Blog">
+      <ul className="m-10">
+        {posts && posts.map((post) => <Post key={post.id} post={post} />)}
+      </ul>
+    </Layout>
+  );
 };
 
 export default Blog;
+
+export async function getStaticProps() {
+  const posts = await getAllPostsData();
+  return {
+    props: { posts },
+  };
+}
